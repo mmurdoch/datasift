dcd166ab561e8c0d5c01c94691edfca2d6af051d
mmurdoch <matthew.murdoch.0@gmail.com>
2015-04-20 22:36:16 +0100

    Add Ctrl+C handling

--- get-characters.py
+++ get-characters.py
@@ -2,9 +2,12 @@ import marvelous
 import sys
 
 
-if len(sys.argv) < 3:
-    print('Usage: python marvelous.py <public_key> <private_key>')
-    exit(0)
+try:
+    if len(sys.argv) < 3:
+        print('Usage: python marvelous.py <public_key> <private_key>')
+        exit(0)
 
-retriever = marvelous.CharacterRetriever(sys.argv[1], sys.argv[2])
-retriever.run()
+    retriever = marvelous.CharacterRetriever(sys.argv[1], sys.argv[2])
+    retriever.run()
+except KeyboardInterrupt:
+    pass
--- marvelous.py
+++ marvelous.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import codecs
+import csv
 import hashlib
 import json
 import os
@@ -8,8 +10,12 @@ import uuid
 
 
 class TimeoutError(Exception):
+    def __init__(self, timeout_seconds):
+        self._timeout_seconds = timeout_seconds
+
     def __str__(self):
-        return 'Request timed out'
+        return 'Request timed out after {0} seconds'.format(
+            self._timeout_seconds)
 
 
 class ResponseError(Exception):
@@ -18,7 +24,7 @@ class ResponseError(Exception):
         self._reason = reason
 
     def __str__(self):
-        return 'Error received: {0} ({1})'.format(
+        return 'Received error: {0} ({1})'.format(
             self._reason, self._status_code)
 
 
@@ -44,29 +50,17 @@ class CharacterRetriever(object):
         md5.update(self._public_key)
         return md5.hexdigest()
 
-    def _get_next_offset(self, filename):
-        match = re.match(self.CHARACTERS_FILE_PREFIX +'\d+-(\d+)$', filename)
-        if match:
-            return int(match.group(1)) + 1
-
-        return None
-
     def _is_character_file(self, filename):
         return filename.startswith(self.CHARACTERS_FILE_PREFIX)
 
-    def _open_character_file(self, start_offset, end_offset):
-        characters_filename = '{0}{1}-{2}'.format(
-            self.CHARACTERS_FILE_PREFIX, start_offset, end_offset)
-        return open(characters_filename, 'w')
+    def _open_character_file(self):
+        return codecs.open('characters.csv', 'a', 'utf-8')
 
     def _extract_character(self, result):
-        return {
-            'name': result['name'],
-            'id': result['id'],
-            'description': result['description'],
-            'comic-count': result['comics']['available'],
-            'story-count': result['stories']['available']
-        }
+        return [
+            #result['name'], result['id'], #result['description'],
+            result['comics']['available'], result['stories']['available']
+        ]
 
     def _get_characters(self, get_function, offset):
         request_id = str(uuid.uuid4())
@@ -78,13 +72,12 @@ class CharacterRetriever(object):
 
         response = None
 
-        # TODO Add proper retry logic
         try:
             response = get_function(
                 self.CHARACTERS_URI, params = parameters,
                 timeout = self.TIMEOUT_SECONDS)
         except requests.exceptions.Timeout:
-            raise TimeoutError()
+            raise TimeoutError(self.TIMEOUT_SECONDS)
 
         status_code = response.status_code
         if status_code != 200:
@@ -92,21 +85,20 @@ class CharacterRetriever(object):
 
         return response
 
-
     def run(self):
         offset = 0
-
-        characters_filenames = filter(self._is_character_file, os.listdir('.'))
-        if characters_filenames:
-            offsets = map(self._get_next_offset, characters_filenames)
-            offset = reduce(max, offsets)
 
         characters_to_get = True
         while characters_to_get:
             print('Retrieving characters {0} to {1}'.format(
                 offset, offset+self.LIMIT-1))
 
-            response = self._get_characters(requests.get, offset)
+            try:
+                response = self._get_characters(requests.get, offset)
+            except TimeoutError as timeout_error:
+                print(str(timeout_error))
+            except ResponseError as response_error:
+                print(str(response_error))
 
             content_json = response.text
             content = json.loads(content_json)
@@ -120,10 +112,11 @@ class CharacterRetriever(object):
 
             next_offset = offset + count
 
-            with self._open_character_file(
-                offset, next_offset-1) as character_file:
-                characters = [self._extract_character(r) for r in results]
-                character_file.write(json.dumps(characters))
+            with self._open_character_file() as character_file:
+                csv_writer = csv.writer(character_file)
+
+                for result in results:
+                    csv_writer.writerow(self._extract_character(result))
 
             offset = next_offset
 
