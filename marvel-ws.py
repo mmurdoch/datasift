571517980f0e907b1d12ac94d7a27a0604b1fb76
mmurdoch <matthew.murdoch.0@gmail.com>
2015-05-14 10:30:42 +0100

    Add stub web service for statistics

--- marvel-ws.py
+++ marvel-ws.py
@@ -4,6 +4,18 @@ from flask import Flask, request, url_for, abort, jsonify, make_response
 from pymongo import MongoClient, ASCENDING
 from pymongo.errors import DuplicateKeyError
 
+class MissingFieldError(Exception):
+    def __init__(self, field_name):
+        self._error_response = self.create_missing_field_error(field_name)
+
+    def create_missing_field_error(self, field_name):
+        return create_error('Missing interaction field: {0}'.format(field_name))
+
+    @property
+    def error_response(self):
+        return self._error_response
+
+
 client = MongoClient()
 
 db = client.marvel
@@ -22,6 +34,15 @@ def remove_mongodb_ids(all_interactions):
 
     return [remove_mongodb_id(i) for i in all_interactions]
 
+def create_error(message):
+    return jsonify({'error': message}), 400
+
+def get_field(request, field_name):
+    if not field_name in request.json:
+       raise MissingFieldError(field_name)
+
+    return request.json[field_name]
+
 @api.route('/marvel/api/v1.0/interactions', methods=['GET'])
 def get_interactions():
     all_interactions = remove_mongodb_ids(interactions.find())
@@ -33,28 +54,6 @@ def get_interaction(id):
     interaction = remove_mongodb_id(interactions.find_one({'id': int(id)}))
 
     return jsonify(interaction), 200
-
-def create_error(message):
-    return jsonify({'error': message}), 400
-
-def create_missing_field_error(field_name):
-    return create_error('Missing interaction field: {0}'.format(field_name))
-
-
-class MissingFieldError(Exception):
-    def __init__(self, field_name):
-        self._error_response = create_missing_field_error(field_name)
-
-    @property
-    def error_response(self):
-        return self._error_response
-
-
-def get_field(request, field_name):
-    if not field_name in request.json:
-       raise MissingFieldError(field_name)
-
-    return request.json[field_name]
 
 @api.route('/marvel/api/v1.0/interactions', methods=['POST'])
 def create_interaction():
@@ -88,6 +87,10 @@ def create_interaction():
     response.headers['Location'] = interaction_uri
 
     return response
+
+@api.route('/marvel/api/v1.0/statistics', methods=['GET'])
+def get_statistics():
+    pass
 
 if __name__ == '__main__':
     api.run(debug=True)
